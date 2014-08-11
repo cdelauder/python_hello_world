6f751e51b1cec2d46756fae85f712bece6b151a3
cdelauder <cdelauder@gmail.com>
2014-08-11 16:04:02 -0700

    have 2 routes with templates working

--- hello.py
+++ hello.py
@@ -1,8 +1,14 @@
 import os
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return render_template('hello.html')
+
+
+@app.route('/wat')
+def wat():
+    return 'WAT!!!'
